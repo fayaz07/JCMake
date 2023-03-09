71712a5b63db24df07e3bc7d52f977ffc5358f1b
Mohammad Fayaz <fayaz.mohammad@thoughtworks.com>
2023-03-09 16:54:57 +0530

    add support for showing error on textfield

---
+++ jcmake/src/main/java/dev/mfayaz/jcmake/ui/DefaultTextFieldWithError.kt
@@ -0,0 +1,37 @@
+package dev.mfayaz.jcmake.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import dev.mfayaz.jcmake.style.COLOR_ERROR
+
+@Composable
+fun DefaultTextFieldWithError(
+  label: String,
+  value: String,
+  onChange: (String, String) -> Unit,
+  keyboardType: KeyboardType = KeyboardType.Text,
+  error: String = ""
+) {
+  Column {
+    DefaultTextField(
+      label = label,
+      value = value,
+      onChange = onChange,
+      keyboardType = keyboardType,
+    )
+    Text(
+      modifier = Modifier.padding(top = 4.dp, start = 8.dp),
+      text = error,
+      style = TextStyle(
+        color = COLOR_ERROR
+      )
+    )
+  }
+}
