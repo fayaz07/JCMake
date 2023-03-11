486b2fc096a3b197e3a2495d5f61b959ae18610c
Mohammad Fayaz <fayazfz07@gmail.com>
2023-03-11 15:20:32 +0530

    fix test cases

--- jcmake/src/test/java/dev/mfayaz/jcmake/parser/JSONUIParserTest.kt
+++ jcmake/src/test/java/dev/mfayaz/jcmake/parser/JSONUIParserTest.kt
@@ -6,15 +6,19 @@ import dev.mfayaz.jcmake.exceptions.JSONParseException
 import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldBe
+import org.json.JSONObject
 import org.junit.Before
 import org.junit.Test
 
 class JSONUIParserTest {
 
   private lateinit var simpleJsonParser: JSONUIParser
+  private lateinit var jsonObject: JSONObject
+
   @Before
   fun setup() {
     simpleJsonParser = JSONUIParser(simpleForm1)
+    jsonObject = JSONObject(simpleForm1)
   }
 
   @Test
@@ -47,46 +51,46 @@ class JSONUIParserTest {
 
   @Test
   fun `when passed a key to typeOf which holds a String value should return String field type`() {
-    simpleJsonParser.typeOf("name") shouldBe FieldType.String
+    simpleJsonParser.typeOf("name", jsonObject) shouldBe FieldType.String
   }
 
   @Test
   fun `when passed a key to typeOf which holds a Char value should return String field type`() {
-    simpleJsonParser.typeOf("grade") shouldBe FieldType.String
+    simpleJsonParser.typeOf("grade", jsonObject) shouldBe FieldType.String
   }
 
   @Test
   fun `when passed a key to typeOf which holds a Boolean value should return Boolean field type`() {
-    simpleJsonParser.typeOf("active") shouldBe FieldType.Boolean
+    simpleJsonParser.typeOf("active", jsonObject) shouldBe FieldType.Boolean
   }
 
   @Test
   fun `when passed a key to typeOf which holds a Int value should return Long field type`() {
-    simpleJsonParser.typeOf("age") shouldBe FieldType.Long
+    simpleJsonParser.typeOf("age", jsonObject) shouldBe FieldType.Long
   }
 
   @Test
   fun `when passed a key to typeOf which holds a Long value should return Long field type`() {
-    simpleJsonParser.typeOf("phone") shouldBe FieldType.Long
+    simpleJsonParser.typeOf("phone", jsonObject) shouldBe FieldType.Long
   }
 
   @Test
   fun `when passed a key to typeOf which holds a Float value should return Float field type`() {
-    simpleJsonParser.typeOf("weight") shouldBe FieldType.Double
+    simpleJsonParser.typeOf("weight", jsonObject) shouldBe FieldType.Double
   }
 
   @Test
   fun `when passed a key to typeOf which holds an TextFieldObject should TextFieldObject Object field type`() {
-    simpleJsonParser.typeOf("password") shouldBe FieldType.TextFieldObject
+    simpleJsonParser.typeOf("password", jsonObject) shouldBe FieldType.TextFieldObject
   }
 
   @Test
   fun `when passed a key to typeOf which holds an TextFieldObject should return TextFieldObject field type - 2`() {
-    simpleJsonParser.typeOf("rollNumber") shouldBe FieldType.TextFieldObject
+    simpleJsonParser.typeOf("rollNumber", jsonObject) shouldBe FieldType.TextFieldObject
   }
 
   @Test
   fun `when passed a key to typeOf which holds an TextFieldObject should return TextFieldObject field type - 3`() {
-    simpleJsonParser.typeOf("bmi") shouldBe FieldType.TextFieldObject
+    simpleJsonParser.typeOf("bmi", jsonObject) shouldBe FieldType.TextFieldObject
   }
 }
