ad57cf8d43807abe893862b59f5c69fa03fe55ee
prudhvir3ddy <prudhvi.reddy@target.com>
2023-03-14 00:20:26 +0530

    deleted unused file

--- jcmake/src/main/java/dev/mfayaz/jcmake/model/ui/ComposableStyling.kt
+++
@@ -1,9 +0,0 @@
-package dev.mfayaz.jcmake.model.ui
-
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.ui.unit.dp
-
-data class ComposableStyling(
-  val fillWidth: Boolean = false,
-  val padding: PaddingValues = PaddingValues(0.dp),
-)
