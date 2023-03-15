20e6ca2e12b40026bc4fc70cb796a1b965cafa5d
Mohammad Fayaz <fayaz.mohammad@thoughtworks.com>
2023-03-16 00:06:04 +0530

    add support for pulling updated json data at host level

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This library takes JSON as input and produces Jetpack Compose's Composables as o
 
 ## Development Environment
 
-![Android Studio Version](https://img.shields.io/badge/android--studio-flamingo-success) ![Gradle Version](https://img.shields.io/badge/gradle-v8.0-blue) ![Kotlin Version](https://img.shields.io/badge/kotlin-v1.7.20-blue) ![AGP](https://img.shields.io/badge/agp-v8.1.0-alpha08-blue)
+![Android Studio Version](https://img.shields.io/badge/android--studio-giraffee--c9-success) ![Gradle Version](https://img.shields.io/badge/gradle-v8.0-blue) ![Kotlin Version](https://img.shields.io/badge/kotlin-v1.7.20-blue) ![AGP](https://img.shields.io/badge/agp-v8.1.0-alpha09-blue)
 
 ## Supported Composables
 
--- app/src/main/java/dev/mfayaz/jcmakedemo/MainActivity.kt
+++ app/src/main/java/dev/mfayaz/jcmakedemo/MainActivity.kt
@@ -1,15 +1,27 @@
 package dev.mfayaz.jcmakedemo
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import dev.mfayaz.jcmake.MakeUI
+import dev.mfayaz.jcmake.parser.JSONUIParser
 import dev.mfayaz.jcmakedemo.data.TestData
 import dev.mfayaz.jcmakedemo.ui.theme.JCMakeTheme
 
@@ -34,7 +46,29 @@ fun onDataChange(key: String, value: Any) {
   Log.d("change", "key=$key, value=$value")
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Content() {
-  MakeUI(TestData.nestedJsonLevel4, ::onDataChange)
+  val jsonUiParser = remember { JSONUIParser(TestData.nestedJsonLevel4) }
+  Scaffold(
+    topBar = {
+      TopAppBar(
+        title = { Text(text = "JCMake demo") },
+        actions = {
+          IconButton(onClick = {
+            Log.d("json-print", jsonUiParser.rootJsonObject.toString())
+          }) {
+            Icon(Icons.Default.Send, "send")
+          }
+        }
+      )
+    }
+  ) {
+    MakeUI(
+      modifier = Modifier.padding(it),
+      jsonUiParser = jsonUiParser,
+      onDataChange = ::onDataChange
+    )
+  }
 }
