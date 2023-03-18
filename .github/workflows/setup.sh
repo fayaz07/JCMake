3be47957737f07cab0ed3a80981dd079440e4ee5
Mohammad Fayaz <fayazfz07@gmail.com>
2023-03-18 14:11:51 +0530

    add gh workflow for publishing artifact

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -6,7 +6,7 @@ PROPERTIES_FILE="local.properties"
 
 touch $PROPERTIES_FILE
 
-echo "gpr.usr=gh-user" >> $PROPERTIES_FILE
-echo "gpr.key=token" >> $PROPERTIES_FILE
+echo "gpr.usr=$1" >> $PROPERTIES_FILE
+echo "gpr.key=$2" >> $PROPERTIES_FILE
 
 echo "Properties file created at $PROPERTIES_FILE"
