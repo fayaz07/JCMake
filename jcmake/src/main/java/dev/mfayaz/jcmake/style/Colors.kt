fd21d2419157c72a88e63a88b06669623897a3be
Mohammad Fayaz <fayazfz07@gmail.com>
2023-03-23 12:35:44 +0530

    implement pretty json composable

--- jcmake/src/main/java/dev/mfayaz/jcmake/style/Colors.kt
+++ jcmake/src/main/java/dev/mfayaz/jcmake/style/Colors.kt
@@ -2,4 +2,8 @@ package dev.mfayaz.jcmake.style
 
 import androidx.compose.ui.graphics.Color
 
-val COLOR_ERROR = Color(0xffe90000)
+val COLOR_ERROR = Color(0xFFE90000)
+val COLOR_ORANGE = Color(0xFFD87705)
+val COLOR_GREEN = Color(0xFF10AC32)
+val COLOR_SKYBLUE = Color(0xFF1B9CC5)
+val COLOR_BLUE = Color(0xFF0B55E4)
